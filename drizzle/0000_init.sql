c6953b1d7eca7c33d050a80aea8ebd4ebf497e97
Michael Holley <michael.philipp.holley@gmail.com>
2025-07-14 09:08:05 +0200

    updated schema

--- drizzle/0000_init.sql
+++ drizzle/0000_init.sql
@@ -4,6 +4,14 @@ CREATE TABLE "ingredients" (
 	"recipe_id" integer NOT NULL
 );
 --> statement-breakpoint
+CREATE TABLE "instructions" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"heading" text,
+	"instructions" text NOT NULL,
+	"step_order" integer NOT NULL,
+	"recipe_id" integer NOT NULL
+);
+--> statement-breakpoint
 CREATE TABLE "recipes" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"name" text NOT NULL,
@@ -11,4 +19,5 @@ CREATE TABLE "recipes" (
 	"created_at" timestamp DEFAULT now()
 );
 --> statement-breakpoint
-ALTER TABLE "ingredients" ADD CONSTRAINT "ingredients_recipe_id_recipes_id_fk" FOREIGN KEY ("recipe_id") REFERENCES "public"."recipes"("id") ON DELETE cascade ON UPDATE no action;
+ALTER TABLE "ingredients" ADD CONSTRAINT "ingredients_recipe_id_recipes_id_fk" FOREIGN KEY ("recipe_id") REFERENCES "public"."recipes"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "instructions" ADD CONSTRAINT "instructions_recipe_id_recipes_id_fk" FOREIGN KEY ("recipe_id") REFERENCES "public"."recipes"("id") ON DELETE cascade ON UPDATE no action;
