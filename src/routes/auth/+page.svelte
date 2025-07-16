6ce6965a71a2dff6b2baebe70a4463c7d40c518f
Michael Holley <michael.philipp.holley@gmail.com>
2025-07-16 09:55:16 +0200

    show error on form submit

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,8 +1,11 @@
-<script>
+<script lang="ts">
 	import { enhance } from '$app/forms';
 	import { Button } from '$lib/components/ui/button';
 	import { Input } from '$lib/components/ui/input';
 	import LoginIcon from '@lucide/svelte/icons/log-in';
+	import type { ActionData } from '../$types';
+
+	const { form } = $props<{ form: ActionData }>();
 </script>
 
 <form method="POST" class="mx-auto flex max-w-sm flex-col gap-4" use:enhance>
@@ -10,4 +13,8 @@
 	<div class="flex flex-row justify-center">
 		<Button type="submit"><LoginIcon />Login</Button>
 	</div>
+
+	{#if form?.message}
+		<p class="text-center text-red-500">{form.message}</p>
+	{/if}
 </form>
