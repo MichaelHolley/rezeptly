083fc8b6e628ebfbea5ea65dab16403550e02fe3
Michael Holley <michael.philipp.holley@gmail.com>
2025-08-19 14:54:07 +0200

    fix

--- src/routes/(app)/[id]/page.remote.ts
+++ src/routes/(app)/[id]/page.remote.ts
@@ -2,13 +2,23 @@ import { command, query } from '$app/server';
 import * as recipeService from '$lib/server/services';
 import { z } from 'zod';
 
-export const deleteRecipe = command(z.number(), async (id) => {
-	if (!id || isNaN(Number(id))) {
-		return fail(400);
-	}
+export const getRecipeById = query(z.number(), async (id) => {
+	return await recipeService.getRecipeById(id);
+});
 
+export const deleteRecipe = command(z.number(), async (id) => {
 	await recipeService.deleteRecipe(id);
 });
+
+// TODO not used yet
+// export const addIngredient = command(
+// 	z.object({ recipeId: z.number(), name: z.string() }),
+// 	async ({ recipeId, name }) => {
+// 		await recipeService.createIngredient({ name, recipeId });
+
+// 		getRecipeById(recipeId).refresh();
+// 	}
+// );
 
 export const removeIngredient = command(
 	z.object({ recipeId: z.number(), ingrId: z.number() }),
