b637d9bea699918f908ea1d31f68b835411b7359
Michael Holley <michael.philipp.holley@gmail.com>
2025-09-07 10:40:54 +0200

    updated tag component

--- src/lib/components/recipes/TagComponent.svelte
+++ src/lib/components/recipes/TagComponent.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
 	import { Badge } from '$lib/components/ui/badge';
 
-	const { tag, onClick } = $props<{ tag: string; onClick?: () => void }>();
+	const { tag, onClick, active } = $props<{
+		tag: string;
+		onClick?: () => void;
+		active?: boolean;
+	}>();
 </script>
 
-<Badge variant="secondary" class={onClick && 'hover:cursor-pointer'} onclick={onClick}>{tag}</Badge>
+{#if active}
+	<Badge class={onClick && 'hover:cursor-pointer'} onclick={onClick}>
+		{tag}
+	</Badge>
+{:else}
+	<Badge variant="secondary" class={onClick && 'hover:cursor-pointer'} onclick={onClick}>
+		{tag}
+	</Badge>
+{/if}
