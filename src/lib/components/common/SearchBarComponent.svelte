f340aba02ffcde9bd7860adb3b882268f1d3632b
Michael Holley <michael.philipp.holley@gmail.com>
2025-11-07 16:41:16 +0100

    feat(Client): added searchbar component
    closes #14

---
+++ src/lib/components/common/SearchBarComponent.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Input from '$lib/components/ui/input/input.svelte';
+
+	let {
+		placeholder = 'Search...',
+		class: className,
+		searchTerm = $bindable()
+	} = $props<{ searchTerm: string; placeholder?: string; class?: string }>();
+</script>
+
+<Input
+	data-slot="search-bar"
+	class={`border-orange-500/50 bg-orange-500/2 placeholder:text-orange-500/60 focus-visible:ring-orange-500/20 ` +
+		className}
+	{placeholder}
+	bind:value={searchTerm}
+/>
