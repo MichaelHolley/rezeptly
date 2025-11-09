ee050fda3bbbf7e48785f7e8bf472b5083a11660
Michael Holley <michael.philipp.holley@gmail.com>
2025-11-09 15:00:15 +0100

    feat: improved mobile display

--- src/lib/components/common/RezeptlyHeaderComponent.svelte
+++ src/lib/components/common/RezeptlyHeaderComponent.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <a href="/" class="flex flex-row items-center gap-3">
-	<UtensilsIcon class="stroke-foreground h-8 w-8 stroke-3 drop-shadow-sm" />
-	<h1 class="font-styled pt-2 text-orange-500 text-shadow-sm">Rezeptly</h1>
+	<UtensilsIcon class=" sm:stroke-foreground h-8 w-8 stroke-orange-500 stroke-3 drop-shadow-sm" />
+	<h1 class="font-styled hidden pt-2 text-orange-500 text-shadow-sm sm:block">Rezeptly</h1>
 </a>
