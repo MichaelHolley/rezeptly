7702cf82ff733e35b47eee3fa26255fca9404d69
Michael Holley <michael.philipp.holley@gmail.com>
2025-11-16 21:12:28 +0100

    minor fix

--- src/lib/components/common/LoadingComponent.svelte
+++ src/lib/components/common/LoadingComponent.svelte
@@ -1,11 +1,6 @@
 <script lang="ts">
 	import { cn } from '$lib/utils';
 	import LoaderIcon from '@lucide/svelte/icons/loader';
-
-	const { hidden }: { hidden?: boolean } = $props();
 </script>
 
-<LoaderIcon
-	class={cn('animate-spin text-neutral-400', hidden && 'hidden')}
-	style="animation-duration: 2s;"
-/>
+<LoaderIcon class={cn('animate-spin text-neutral-400')} style="animation-duration: 2s;" />
