59bf4d85c596b485b6c15d5df26f16e0c2e63afc
Michael Holley <michael.philipp.holley@gmail.com>
2025-12-06 16:33:03 +0100

    broken preview url

---
+++ src/lib/components/common/BrokenImagePreview.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import * as Empty from '$lib/components/ui/empty/';
+	import BugIcon from '@lucide/svelte/icons/bug';
+</script>
+
+<Empty.Root>
+	<Empty.Header>
+		<Empty.Media variant="icon">
+			<BugIcon class="text-neutral-300" />
+		</Empty.Media>
+		<Empty.Description class="text-neutral-300">Preview broken</Empty.Description>
+	</Empty.Header>
+</Empty.Root>
