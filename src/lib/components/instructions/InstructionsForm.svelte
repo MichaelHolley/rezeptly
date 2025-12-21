1a15a17e0f45ca94f50ef0220a0d99b577e93605
Michael Holley <michael.philipp.holley@gmail.com>
2025-12-21 15:19:48 +0100

    fix: lint issues with typing

--- src/lib/components/instructions/InstructionsForm.svelte
+++ src/lib/components/instructions/InstructionsForm.svelte
@@ -10,13 +10,11 @@
 	import PlusIcon from '@lucide/svelte/icons/plus';
 	import TrashIcon from '@lucide/svelte/icons/trash-2';
 	import { onMount } from 'svelte';
-	import z from 'zod';
 
-	type Step = z.infer<typeof StepSchema>;
-	const StepSchema = z.object({
-		heading: z.string().nullable(),
-		description: z.string()
-	});
+	type Step = {
+		heading: string | null;
+		description: string;
+	};
 
 	const {
 		recipe,
