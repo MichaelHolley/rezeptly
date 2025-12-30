8778169514c893fb6a636ed8ef6ff002d358dc90
Michael Holley <michael.philipp.holley@gmail.com>
2025-12-30 15:32:02 +0100

    feat: improved rezeptly page header

--- src/lib/components/common/navigation/RezeptlyHeaderComponent.svelte
+++ src/lib/components/common/navigation/RezeptlyHeaderComponent.svelte
@@ -3,6 +3,15 @@
 </script>
 
 <a href="/" class="flex flex-row items-center gap-3">
-	<UtensilsIcon class="sm:stroke-foreground size-8 stroke-orange-500 stroke-3 drop-shadow-sm" />
+	<div class="p-2 rounded-full bg-orange-500 drop-shadow-md">
+		<UtensilsIcon class="size-6 sm:size-8 stroke-3 drop-shadow-sm stroke-neutral-100" />
+	</div>
 	<h1 class="font-modak hidden pt-2 text-orange-500 text-shadow-md sm:block">rezeptly</h1>
 </a>
+
+<style>
+	h1 {
+		-webkit-text-fill-color: var(--color-neutral-100);
+		-webkit-text-stroke: 3px var(--color-orange-500);
+	}
+</style>
