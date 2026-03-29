4d1b442df9cfaea4082a7e248ac00fef5e41684f
Michael Holley <michael.philipp.holley@gmail.com>
2026-03-29 19:48:43 +0200

    feat: improve constraints

--- drizzle/0009_TagCategoryNotNull.sql
+++
@@ -1,7 +0,0 @@
--- Remove recipe associations for any remaining uncategorized tags
-DELETE FROM "recipes_to_tags" WHERE "tag_id" IN (SELECT "id" FROM "tags" WHERE "category" IS NULL);--> statement-breakpoint
--- Remove any remaining uncategorized tags
-DELETE FROM "tags" WHERE "category" IS NULL;--> statement-breakpoint
-ALTER TABLE "tags" ALTER COLUMN "category" SET NOT NULL;--> statement-breakpoint
-CREATE UNIQUE INDEX "tags_slug_unique" ON "tags" USING btree ("slug");--> statement-breakpoint
-ALTER TABLE "tags" ADD CONSTRAINT "tags_slug_category_unique" UNIQUE("slug","category");
